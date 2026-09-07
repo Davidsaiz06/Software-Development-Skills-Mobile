1635a9241b1219e88d4bead3962c53624a92a178
david <saizmolina06@gmail.com>
2026-09-07 12:12:42 +0300

    feat: complete Part 1 with addition UI and Java logic
    - Initialize MainActivity.java and activity_main.xml.
    - Design UI using ConstraintLayout with properly anchored EditText and Button components.
    - Resolve AndroidManifest.xml layout misconfiguration.
    - Implement Java logic to capture user input, validate empty fields, and calculate the sum.
    Pending tasks & Bug fixes:
    - Review and fix edge-case bugs (e.g., multiple decimal points handling).

--- Coursework/Activity 1/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ Coursework/Activity 1/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.myfirstapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        EditText editNum1 = findViewById(R.id.editNum1);
+        EditText editNum2 = findViewById(R.id.editNum2);
+        Button btnAdd = findViewById(R.id.btnAdd);
+        TextView tvResult = findViewById(R.id.tvResult);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strNum1 = editNum1.getText().toString();
+                String strNum2 = editNum2.getText().toString();
+
+                if (!strNum1.isEmpty() && !strNum2.isEmpty()) {
+                    double num1 = Double.parseDouble(strNum1);
+                    double num2 = Double.parseDouble(strNum2);
+                    double sum = num1 + num2;
+                    tvResult.setText(String.valueOf(sum));
+                } else {
+                    tvResult.setText("Numbers are missing");
+                }
+            }
         });
     }
 }
